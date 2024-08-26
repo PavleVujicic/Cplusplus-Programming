b85e8f8cc6adfc277f06a6389dcf968170c0ac2f
Pavle Vujicic <125823553+PavleVujicic@users.noreply.github.com>
2024-08-25 21:32:10 -0400

    Introduction to c++ programming

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    int favorite_number;
+    
+    std::cout << "Enter your Favorite number between 1 and 100:";
+    
+    std::cin >> favorite_number;
+    
+    std::cout << "Amazing!! That is my favorite number too!" << std::endl;
+    
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    
+    return 0;
+    
+}
+
